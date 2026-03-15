639d38db8c03aa10ef19f0f0968b7a709ab038fe
siyijiang233 <siyijiang233@gmail.com>
2026-03-16 00:55:47 +0800

    feat: adapt to edgeone pages functions

---
+++ functions/index.ts
@@ -0,0 +1,7 @@
+﻿import { renderDemoPage } from '../src/page';
+
+export async function onRequest(context: any): Promise<Response> {
+  return new Response(renderDemoPage(), {
+    headers: { 'Content-Type': 'text/html; charset=utf-8' },
+  });
+}
---
+++ functions/v1/models.ts
@@ -0,0 +1,9 @@
+﻿import { handleModels } from '../../../src/models';
+
+export async function onRequest(context: any): Promise<Response> {
+  const { request } = context;
+  if (request.method === 'GET') {
+    return handleModels();
+  }
+  return new Response('Method Not Allowed', { status: 405 });
+}
